c354dc1dfbb4f18ca57aab27e0e264ef55c1e504
Abhay Kukade <kukade.abhay@gmail.com>
2025-05-24 02:04:36 +0000

    v1.10.0

--- custom_transformer.py
+++ custom_transformer.py
@@ -27,3 +27,24 @@ class MultiLabelBinarizerTransformer(BaseEstimator, TransformerMixin):
     
     def get_feature_names_out(self):
         return [f'{col}_{cls}' for col, mlb in self.encoders.items() for cls in mlb.classes_]
+
+
+class EngineSizeTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.engine_map = {
+            '0 - 499 cc': 250,
+            '500 - 999 cc': 750,
+            '1,000 - 1,999 cc': 1500,
+            '2,000 - 2,999 cc': 2500,
+            '3,000 - 3,999 cc': 3500,
+            '4,000 - 4,999 cc': 4500,
+            '5,000 - 5,999 cc': 5500,
+            'More than 6,000 cc': 6500
+        }
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        return X.squeeze().map(self.engine_map).fillna(0).values.reshape(-1, 1)
+        
