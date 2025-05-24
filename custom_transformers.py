1fb17682f1a86bfdefc598fd797c70dcad5c1844
Abhay Kukade <117824563+abhaykukade@users.noreply.github.com>
2025-05-24 08:57:14 -0400

    map km cat to num

--- custom_transformers.py
+++ custom_transformers.py
@@ -66,3 +66,49 @@ class YearTransformer(BaseEstimator, TransformerMixin):
         cleaned = X.squeeze().map(self.year_map)
         trf = pd.to_numeric(cleaned, errors='coerce').fillna(self.mode_)
         return trf.values.reshape(-1, 1)
+
+
+    # kilometers_map = {}
+
+    # for km in train_df['kilometers'].dropna().unique():
+    #     cleaned = km.replace(',', '').replace('+', '')
+    #     val = cleaned.split(' - ')
+    #     if len(val) > 1:
+    #         km_mean = np.mean([int(val[0]), int(val[1]) + 1])
+    #     else:
+    #         km_mean = int(cleaned)
+    #     kilometers_map[km] = km_mean
+class KilometersTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.km_map = {'100,000 - 109,999': 105000.0,
+                        '0': 0,
+                        '20,000 - 29,999': 25000.0,
+                        '+200,000': 200000,
+                        '190,000 - 199,999': 195000.0,
+                        '40,000 - 49,999': 45000.0,
+                        '50,000 - 59,999': 55000.0,
+                        '150,000 - 159,999': 155000.0,
+                        '170,000 - 179,999': 175000.0,
+                        '130,000 - 139,999': 135000.0,
+                        '110,000 - 119,999': 115000.0,
+                        '60,000 - 69,999': 65000.0,
+                        '80,000 - 89,999': 85000.0,
+                        '30,000 - 39,999': 35000.0,
+                        '90,000 - 99,999': 95000.0,
+                        '120,000 - 129,999': 125000.0,
+                        '10,000 - 19,999': 15000.0,
+                        '1 - 999': 500,
+                        '1,000 - 9,999': 5500.0,
+                        '70,000 - 79,999': 75000.0,
+                        '140,000 - 149,999': 145000.0,
+                        '160,000 - 169,999': 165000.0,
+                        '180,000 - 189,999': 185000.0
+                        }
+
+    def fit(self, X, y=None):
+        self.mode_ = X.squeeze().map(self.km_map).mode()[0]
+        return self
+    
+    def transform(self, X):
+        return X.squeeze().map(self.km_map).fillna(self.mode_).values.reshape(-1, 1)
+    
